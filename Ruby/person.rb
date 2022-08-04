9cb4d2e3eafbeb45216b8d5855878443f58ba395
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-04 19:58:03 +0000

    create relationships between classes, add rentals to class person

--- Ruby/person.rb
+++ Ruby/person.rb
@@ -1,6 +1,7 @@
 require_relative './decorate'
 
 class Person < Nameable
+  @rentals = []
   def initialize(age, name = 'Unknown', parent_permision = true)
     super()
     @id = Random.rand(1..10_000)
@@ -27,6 +28,11 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permision
+  end
+
+  # add rentals 
+  def add_rental rental
+    rentals << rental
   end
 
   private
