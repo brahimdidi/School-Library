628ed98ccf7654bfd7114d08e1024dd296c9a593
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-05 11:37:43 +0000

    add rentals array and its accessor , fix bugs

--- Ruby/book.rb
+++ Ruby/book.rb
@@ -1,9 +1,15 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
-  @rental = Rental
-  def intialize(title, author)
+  def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+
 end
