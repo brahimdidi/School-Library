c307e209486ab7953401047815113284b7799673
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-05 11:40:52 +0000

    push students to classroom when added, fix bugs, test relationship

--- Ruby/student.rb
+++ Ruby/student.rb
@@ -1,12 +1,19 @@
 require_relative './person'
 
 class Student < Person
+  attr_accessor :classroom
   def initialize(classroom)
     super
     @classroom = classroom
+    classroom.students.push(self)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
