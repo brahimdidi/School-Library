7fb4ee26e81919de145f39cc423194248227a729
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-05 11:51:08 +0000

    remove book and person method (we don't need them)

--- Ruby/rental.rb
+++ Ruby/rental.rb
@@ -1,22 +1,11 @@
 class Rental
-  attr_accessor :date , :book, :person
+  attr_accessor :date, :book, :person
 
-  def initialize(person,book,date)
+  def initialize(person, book, date)
     @date = date
     @person = person
     person.rentals << self
     @book = book
     book.rentals << self
   end
-  
-  def book=(book)
-    @book = book
-    book.rentals.push(self) unless book.rentals.include?(self)
-  end
-
-  def person=(person)
-    @person = person
-    person.rentals.push(self) unless person.rentals.include?(self)
-  end
-
 end
