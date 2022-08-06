9a1955135c00df0781b6734b7ad938678de2e431
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-06 02:24:13 +0000

    push new teachers to teachers class var array add setter for it

--- Ruby/teacher.rb
+++ Ruby/teacher.rb
@@ -1,9 +1,23 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(specialization)
-    super
-    @specialization = specialization
+  attr_accessor :specialization
+
+  @@teachers = []
+  @@count = 0
+  def initialize(specitalization, age, name, profession = 'Teacher')
+    super(age, name, profession)
+    @specitalization = specitalization
+    @@teachers << { :ID => @id, :name => name, :age => age, :profession => '[Teacher]', :self => self }
+    @@count += 1
+  end
+
+  def self.teachers
+    @@teachers
+  end
+
+  def self.count
+    @@count
   end
 
   def can_use_services?
