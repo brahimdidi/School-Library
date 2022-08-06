ee65f3894a57c8203516ffe3b23df864919787aa
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-06 02:24:34 +0000

    remove unesed test code

--- Ruby/teacher.rb
+++ Ruby/teacher.rb
@@ -4,21 +4,17 @@ class Teacher < Person
   attr_accessor :specialization
 
   @@teachers = []
-  @@count = 0
   def initialize(specitalization, age, name, profession = 'Teacher')
     super(age, name, profession)
     @specitalization = specitalization
     @@teachers << { :ID => @id, :name => name, :age => age, :profession => '[Teacher]', :self => self }
-    @@count += 1
+
   end
 
   def self.teachers
     @@teachers
   end
 
-  def self.count
-    @@count
-  end
 
   def can_use_services?
     true
